482db789737067411af4b316d631d9fd7a795553
Macbook Air <macbookair@MacBook-Pro.local>
2024-08-05 22:22:43 +0500

    Initial Commit

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <h1>I am Your Footer</h1>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className=" bg-green">
+      <h1>I am Your Header</h1>
+    </div>
+  );
+};
+
+export default Header;
